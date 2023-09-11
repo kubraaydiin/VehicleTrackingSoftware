0074dfa4e3ef495348a03f7e7fb2fbe8da711fd0
kubraaydiin <68286626+kubraaydiin@users.noreply.github.com>
2023-09-11 18:51:18 +0300

    enhancements

--- VehicleTrackingSoftware/wwwroot/js/site.js
+++ VehicleTrackingSoftware/wwwroot/js/site.js
@@ -1,4 +1,43 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function calculate(fromCity, toCity) {
+    let request = {
+        origin: fromCity,
+        destination: toCity,
+        travelMode: google.maps.TravelMode.DRIVING,
+    }
 
-// Write your JavaScript code.
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            const distance = document.querySelector('#distance');
+            const duration = document.querySelector('#duration');
+            distance.innerHTML = "Mesafe: " + result.routes[0].legs[0].distance.text;
+            duration.innerHTML = "Süre: " + result.routes[0].legs[0].duration.text;
+            directionsDisplay.setDirections(result);
+        }
+    })
+}
+
+let myLocation = {
+    lat: 41.0082,
+    lng: 28.9784
+}
+
+let mapOptions = {
+    center: myLocation,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+
+let map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+directionsDisplay.setMap(map);
+
+var parentElement = document.querySelector("#items");
+var rowChildren = document.querySelectorAll(".car-item");
+rowChildren.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+        calculate(element.children[1].outerText, element.children[2].outerText)
+    });
+});
